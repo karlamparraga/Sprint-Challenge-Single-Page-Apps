5c539177a960bb770a1a8a6e66d2344504deaa9e
karlie0 <kamabrapa88@gmail.com>
2019-08-18 23:09:28 -0400

    Added Home Page route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import TabNav from "./components/TabNav.js";
+// import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+// import AppRouter from "./components/AppRouter.js";
+import {Route} from "react-router-dom"
+import CharacterCard from "./components/CharacterCard.js"
+import WelcomePage from "./components/WelcomePage.js";
+
 
 export default function App() {
+ 
+
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+      <Route path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterCard} />
+      {/* <TabNav /> */}
+      {/* <AppRouter /> */}
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard() {
+
+  const [characters, setCharacter] = useState([])
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+    .then(res => res.json())
+    .then(characters => {
+      console.log("characters: ", characters);
+      setCharacter(characters.name);
+    })
+    .catch(err => console.log("error fetching data"))
+  }, []);
+
+  return (
+  
+  <span>todo: character</span>);
 }
+
+export default CharacterCard;
