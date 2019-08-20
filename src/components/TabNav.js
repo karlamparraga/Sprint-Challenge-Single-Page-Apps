15559070b69958e7c8469c08d4bdd6c897c7cde2
karlie0 <kamabrapa88@gmail.com>
2019-08-20 13:06:47 -0400

    Finished MVP

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,40 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import WelcomePage from "./WelcomePage.js";
+import CharacterList from "./CharacterList.js";
+import LocationList from "./LocationsList.js";
+import EpisodeList from "./EpisodeList.js"
 
 // TODO: Add missing tabs below
 export default function TabNav() {
+
+    const panes = [
+        {
+            menuItem: { 
+                content: 'Home Page',
+                icon: 'home'
+            }, render: () => <Tab.Pane><WelcomePage/></Tab.Pane>
+        },
+        { menuItem: {
+            content:'Characters', 
+            icon: 'users'
+        }, render: () => <Tab.Pane><CharacterList/></Tab.Pane> },
+        { menuItem: {
+            content: 'Locations',
+            icon: 'map'
+        }, render: () => <Tab.Pane><LocationList /></Tab.Pane> },
+        { menuItem: {
+            content: 'Episodes',
+            icon: 'video camera'
+        }, render: () => <Tab.Pane><EpisodeList /></Tab.Pane> },
+      ]
+      console.log(panes[1]);
+    return (
+        <div>
+            <Tab panes={panes} />
+        </div>
+        
+    );
 
 };
